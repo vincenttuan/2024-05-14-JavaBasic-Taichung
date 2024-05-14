6e2653a259e5fe4c6ea79e2a79e8d36d6b0e3f0f
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-14 21:05:39 +0800

    Add files via upload

---
+++ javacode/day1/DogFace.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class DogFace {
+	public static void main(String[] args) {
+		System.out.println(" / \\__");
+		System.out.println("(    @\\____");
+		System.out.println(" /         O");
+		System.out.println("/   (_____/");
+		System.out.println("/_____/   U");
+	}
+}
