d533d05ab5721a23a1d17e4e0b99340e7ee2ab52
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-16 19:01:07 +0800

    Add files via upload

---
+++ day2/Demo1.java
@@ -0,0 +1,16 @@
+package day2;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// 宣告一個整數變數 x, 內容初始值為 100
+		int x = 100;
+		System.out.println(x); // 印出 x 變數的內容
+		// 宣告一個整數變數 y, 內容初始值為 50
+		int y = 50;
+		System.out.println(y); // 印出 y 變數的內容
+		
+
+	}
+
+}
