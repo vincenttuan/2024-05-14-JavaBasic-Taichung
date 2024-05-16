d4133547cd5c52b117e9ee961785e7434d113343
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-16 21:09:24 +0800

    Add files via upload

--- day2/Demo7.java
+++ day2/Demo7.java
@@ -4,10 +4,10 @@ public class Demo7 {
 
 	public static void main(String[] args) {
 		// 判斷分數是否及格 ?
-		// 國文成績 = 75, 英文成績 = 45
+		// 國文成績 = 75, 英文成績 = 40
 		// 試問國文與英文是否及格 ?
 		int chinese = 75;
-		int english = 45;
+		int english = 40;
 
 		boolean chineseIsPass = chinese >= 60;
 		boolean englishIsPass = english >= 60;
@@ -19,7 +19,10 @@ public class Demo7 {
 		//-------------------------------------
 		System.out.printf("國文 %d 及格 %b%n", chinese, chineseIsPass);
 		System.out.printf("英文 %d 及格 %b%n", english, englishIsPass);
-		
+		// 請算出總分與平均(小數點1位)各為多少?並印出
+		int sum = chinese + english;
+		double avg = sum / 2.0;
+		System.out.printf("總分 %d 平均 %.1f%n", sum, avg);
 
 	}
 
