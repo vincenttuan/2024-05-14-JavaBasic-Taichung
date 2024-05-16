4a5a615b1b3b498481410753cf5459e863c43e20
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-16 19:13:45 +0800

    Add files via upload

--- day2/Demo1.java
+++ day2/Demo1.java
@@ -9,8 +9,20 @@ public class Demo1 {
 		// 宣告一個整數變數 y, 內容初始值為 50
 		int y = 50;
 		System.out.println(y); // 印出 y 變數的內容
+		//----------------------------------------
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		//----------------------------------------
+		// 利用 printf 列印格式化
+		// %d 放整數, %,d 含千分號
+		// %f 放浮點數, %.2f 到小數點二位
+		// %c 放字元
+		// %b 放布林值
+		// %s 放字串
+		// %n 換行符號
+		System.out.printf("x = %d%n", x);
+		System.out.printf("y = %d%n", y);
 		
-
 	}
 
 }
