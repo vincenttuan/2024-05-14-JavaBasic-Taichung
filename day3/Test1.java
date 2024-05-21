ff06c47502f0a1ce4ea1b7cc360864d341d52c92
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-21 18:47:33 +0800

    Add files via upload

--- day3/Test1.java
+++ day3/Test1.java
@@ -6,6 +6,29 @@ public class Test1 {
 		// 數學運算子
 		int result = 6/2*(1+2);
 		System.out.printf("6/2*(1+2) = %d%n", result);
+
+		// "%" 求餘數 
+		int a = 15%2; // 1
+		int b = 15%3; // 0
+		// printf 中若要印出 % 要改成 %%, 因為 % 是特殊字元
+		System.out.printf("15%%2 = %d%n", a); // 15%2 = 1
+		System.out.printf("15%%3 = %d%n", b); // 15%3 = 0
+
+		// "/" 除法
+		int c = 15/2; // 7
+		double d = 15/2; // 
+		double e = 15/2.0; // 
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		System.out.println("15/2 = " + c);
+		System.out.println("15/2 = " + d);
+		System.out.println("15/2.0 = " + e);
+
+		System.out.printf("15/2 = %d%n", c);
+		System.out.printf("15/2 = %.1f%n", d);
+		System.out.printf("15/2.0 = %.1f%n", e);
 	}
 
 }
