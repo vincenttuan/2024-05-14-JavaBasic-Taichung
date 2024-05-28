c3248853cebd13365ee6dd6165f1bd364a5fb8a6
VincentTuan <vincentjava@yahoo.com.tw>
2024-05-28 20:27:54 +0800

    Add files via upload

--- day5/DoWhileDemo1.java
+++ day5/DoWhileDemo1.java
@@ -9,12 +9,13 @@ public class DoWhileDemo1 {
 	public static void main(String[] args) {
 		Random random = new Random();
 		int ans = random.nextInt(9) + 1; // 1~9 的隨機數 
-
+		int count = 3; // 最多猜 3 次
 		do {
 			// 使用者在鍵盤上輸入數字
 			Scanner scanner = new Scanner(System.in);
 			System.out.print("請輸入(1~9): ");
 			int guess = scanner.nextInt();
+			count--; // 次數減一
 			// 比對使用者輸入的數字(guess)是否與答案(ans)相同
 			if(guess > ans) {
 				System.out.println("猜大了");
@@ -24,7 +25,7 @@ public class DoWhileDemo1 {
 				System.out.println("猜對了");
 				break;
 			}
-		} while(true);
+		} while(count > 0);
 
 	}
 
